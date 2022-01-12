4ed58f5688b64f81af7e92aa812eaa7c3b2edf03
isaquetdiniz <itd@cin.ufpe.br>
2022-01-12 14:01:48 -0300

    feat: remove solid and bad practicles folders

--- src/original/index.ts
+++
@@ -1,5 +0,0 @@
-import httpServer from "./server"
-
-httpServer.listen(3001, () => {
-    console.log('Server rodando na 3001')
-})
--- src/original/repositories/heroes-repository.ts
+++
@@ -1,9 +0,0 @@
-export class HeroesRepository {
-    getAll() {
-        return 'Batman, Superman, Flash'
-    }
-
-    getById(id: string) {
-        return 'Batman'
-    }
-}
